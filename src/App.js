45f48f9b4036ad3f6aee65f415f5afa1329f707a
EricHayter <eric.hayter@ocsbstudent.ca>
2021-08-26 11:21:00 -0400

    deploy my react app to gh pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      id: '',
+      advice: ''
+    };
+    this.newAdvice = this.newAdvice.bind(this);
+    this.randomBackgroundClr = this.randomBackgroundClr.bind(this);
+  }
+
+  randomBackgroundClr() {
+    document.body.style.background = "hsl(" + Math.round(Math.random() * 360) + ",50%,70%)";
+  }
+
+  newAdvice() {
+    fetch('https://api.adviceslip.com/advice')
+    .then(response => response.json())
+    .then(data => {
+      this.setState({
+        id: data.slip.id,
+        advice: data.slip.advice
+      })
+      this.randomBackgroundClr()
+    })
+  }
+
+  componentDidMount() {
+    this.newAdvice()
+  }
+
+  render () {
+    let msgLen = this.state.advice.length + 13
+    const style = {
+      width: `${msgLen}ch`,
+      animation: `typing ${(msgLen)*0.09}s steps(${msgLen}), blink .5s step-end infinite alternate`,
+    }
+
+    return (
+      <div className="wrapper">
+        <div style={style} className="typing-effect" key={this.state.id}>Advice #{this.state.id}: {this.state.advice}</div>
+        <button className="advice-button" onClick={this.newAdvice}>More Advice</button>
+      </div>
+    );
+  }
+
 }
 
 export default App;
